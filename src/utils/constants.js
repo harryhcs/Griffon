d45f16d03235be55e6d801cedc7cf1c5e9936a0b
Herman Stander <her.stander@gmail.com>
2020-02-19 10:10:52 +0200

    added https

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,10 +3,10 @@ const HASURA_GRAPHQL_ENGINE_HOSTNAME = 'griffonapp.westeurope.azurecontainer.io/
 const scheme = (proto) => (window.location.protocol === 'https:' ? `${proto}s` : proto);
 
 export const GRAPHQL_URL = `${scheme(
-  'http',
+  'https',
 )}://${HASURA_GRAPHQL_ENGINE_HOSTNAME}/v1/graphql`;
 export const REALTIME_GRAPHQL_URL = `${scheme(
-  'ws',
+  'wss',
 )}://${HASURA_GRAPHQL_ENGINE_HOSTNAME}/v1/graphql`;
 export const authClientId = 'nAXDIZAr1oqb8Vkj2PKC8EMfG9ojAYkF';
 export const authDomain = 'ppfcns.eu.auth0.com';
